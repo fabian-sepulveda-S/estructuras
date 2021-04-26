ab193789176dd2cb3e6005d742d31a3ae83f679b
fabian <fabian@DESKTOP-VHARO06>
2021-04-26 16:53:49 -0300

    agregar testPadre, terminar testPertenece, cargarArbolCompletoPorNivel

--- estructuras/src/tests/jerarquicas/TestArbolGenerico.java
+++ estructuras/src/tests/jerarquicas/TestArbolGenerico.java
@@ -5,8 +5,9 @@ import jerarquicas.dinamicas.Arbol;
 public class TestArbolGenerico {
 
     public static void main(String[] args) {
-        testPertenece(10);
-        testMostrarArbolCompleto(3,3);
+        // testPertenece(4,2);
+        // testMostrarArbolCompleto(3,3);
+        testPadre(4, 2);
     }
     
     public static void testMostrarArbolCompleto(int grado, int altura) {
@@ -21,13 +22,62 @@ public class TestArbolGenerico {
         System.out.println(arbol.toString());
     }
     
-    public static void testPertenece(int n) {
+    public static void testPadre(int grado, int altura) {
+        System.out.println("-------------------------------------------------");
+        System.out.println("Test padre");
+        System.out.println("Crear un arbol completo de grado " + grado + " y altura " + altura + ":");
+        Arbol arbol = new Arbol();
+        int cantidad = testCargarArbolCompletoPorNivel(arbol, grado, altura);
+        System.out.println(arbol.toString());
+        System.out.println();
+        
+        System.out.println("Por cada elemento mostrar su padre: ");
+        for(int i = 1; i <= cantidad; i++) {
+            Object padre = arbol.padre(i);
+            System.out.println("padre de " + i + ": " + (padre == null ? "null" : padre.toString()));
+        }
+        System.out.println();
+        
+        System.out.println("para elementos que no pertenecen al arbol retorna null: ");
+        for(int i = cantidad + 1; i <= 2 * cantidad; i++) {
+            Object padre = arbol.padre(i);
+            System.out.println("padre de " + i + ": " + (padre == null ? "null" : padre.toString()));
+        }
+        System.out.println();
+    }
+    
+    public static void testPertenece(int grado, int altura) {
         System.out.println("-------------------------------------------------");
         System.out.println("Test pertenece");
+        
         System.out.println("Crear arbol vacío");
         Arbol arbol = new Arbol();
+        System.out.println();
+        
         System.out.println("pertenece en arbol vacío da false");
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < 10; i++) {
+            System.out.println("pertenece " + i + ": " + arbol.pertenece(i));
+        }
+        System.out.println();
+        
+        System.out.println("Poblar el arbol");
+        int cantidad = testCargarArbolCompletoPreorden(arbol, grado, altura);
+        int cantidadElementos = 1 + grado * ((int) Math.pow(grado, altura) - 1) / (grado - 1);
+        System.out.println(arbol.toString());
+        System.out.println("cantidad de elementos: " + cantidad + " " + cantidadElementos);
+        System.out.println();
+        
+        System.out.println("por cada elemento de 1 a " + cantidad + " pertenece da true:");
+        for(int i = 1; i <= cantidad; i++) {
+            System.out.println("pertenece " + i + ": " + arbol.pertenece(i));
+        }
+        System.out.println();
+        
+        System.out.println("elementos fuera de rango da false: ");
+        for(int i = -1; i >= -cantidad; i--) {
+            System.out.println("pertenece " + i + ": " + arbol.pertenece(i));
+        }
+        for(int i = cantidad + 1; i <= 2 * cantidad; i++) {
             System.out.println("pertenece " + i + ": " + arbol.pertenece(i));
         }
     }
@@ -57,5 +107,24 @@ public class TestArbolGenerico {
             }
         }
         return insertados;
+    }
+
+    public static int testCargarArbolCompletoPorNivel(Arbol arbol, int grado, int altura) {
+        
+        // formula para padre deducida de mirar arboles completos de grado 2, 3,
+        // 4 y altura 3
+        // después de probar con grados y alturas mayores, parece ser que
+        // se cumple en general :)
+        // TODO: buscar demostración de que la formula se cumple en general
+        
+        // formula de la progresión geométrica
+        int cantidadElementos = 1 + grado * ((int) Math.pow(grado, altura) - 1) / (grado - 1);
+        
+        for(int i = 1; i <= cantidadElementos; i++) {
+            int padre = (i + grado - 2) / grado;
+            System.out.print("insertar " + i + ", padre " + padre + ": ");
+            System.out.println(arbol.insertar(i, padre));
+        }
+        return cantidadElementos;
     }
 }
