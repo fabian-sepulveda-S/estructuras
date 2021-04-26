943b1116a15acbf5080cf55ae838120056848025
fabian <fabian@DESKTOP-VHARO06>
2021-04-26 11:46:55 -0300

    implementar metodos NodoArbol

--- estructuras/src/jerarquicas/dinamicas/NodoArbol.java
+++ estructuras/src/jerarquicas/dinamicas/NodoArbol.java
@@ -1,5 +1,38 @@
 package jerarquicas.dinamicas;
 
 public class NodoArbol {
-
+    
+    private Object elem;
+    private NodoArbol hijoIzquierdo;
+    private NodoArbol hermanoDerecho;
+    
+    public NodoArbol(Object elem, NodoArbol hijo, NodoArbol hermano) {
+        this.elem = elem;
+        this.hijoIzquierdo = hijo;
+        this.hermanoDerecho = hermano;
+    }
+    
+    public Object getElem() {
+        return elem;
+    }
+    
+    public NodoArbol getHijoIzquierdo() {
+        return hijoIzquierdo;
+    }
+    
+    public NodoArbol getHermanoDerecho() {
+        return hermanoDerecho;
+    }
+    
+    public void setElem(Object elem) {
+        this.elem = elem;
+    }
+    
+    public void setHijoIzquierdo(NodoArbol izquierdo) {
+        this.hijoIzquierdo = izquierdo;
+    }
+    
+    public void setHermanoDerecho(NodoArbol derecho) {
+        this.hermanoDerecho = derecho;
+    }
 }
